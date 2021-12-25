44cb706885cb6d27db72d240e8cd50b31b781005
Chibuike Uzoechina <62131896+Chike1990@users.noreply.github.com>
2021-12-25 14:37:46 +0100

    Update App.test.js
    Removes commented codes

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,21 +1,3 @@
-// import { render, screen } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import store from '../redux/store/store';
-// import App from '../App';
-
-// test('It should Should display the header', () => {
-//   render(
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </Provider>,
-//   );
-//   const headerText = screen.getByText(/COVID-19 GLOBAL STAT/i);
-//   expect(headerText).toEqual(headerText);
-// });
-
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
