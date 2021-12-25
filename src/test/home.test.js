47bfddeac96490e62ad4d53e9ef8e67f3602fbef
Chibuike Uzoechina <62131896+Chike1990@users.noreply.github.com>
2021-12-25 14:39:06 +0100

    Removes commented codes

--- src/test/home.test.js
+++ src/test/home.test.js
@@ -1,6 +1,3 @@
-// import covidCasesReducer from '../redux/covid/covid-cases-reducer';
-// import setCovidCases from '../redux/covid/covid-cases-actions';
-
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
